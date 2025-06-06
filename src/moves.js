d928744ebc832f7312e92f0c9f8b1e0336dcbaa4
Mahyar <mahyarstate@gmail.com>
2025-06-06 18:40:00 +0330

    add mergeRight() for a row

--- src/moves.js
+++ src/moves.js
@@ -6,3 +6,26 @@ function shiftRight(row) {
 
     return new Array(zeroCount).fill(0).concat(nonZeroItems)
 }
+
+function mergeRight(row) {
+    // if there are zeros between non-zeros
+    // [2, 0, 0, 2] => [0, 0, 2, 2]
+    row = shiftRight(row)
+
+    // merge adjacent equal elements from right to left
+    // [0, 0, 2, 2] => [0, 0, 0, 4]
+    for (let i = row.length - 1; i > 0; i--) {
+        if (row[i] === row[i - 1]) {
+            row[i] *= 2
+            row[i - 1] = 0
+            // skip the next element to avoid double merging
+            i--
+        }
+    }
+
+    // to shift the merged elements again
+    // [2, 2, 2, 2] => [0, 4, 0, 4] => [0, 0, 4, 4]
+    row = shiftRight(row)
+
+    return row
+}
