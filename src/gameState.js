c677e1317df5e14c46a97faef144fca2febbe43f
Mahyar <mahyarstate@gmail.com>
2025-06-08 02:21:33 +0330

    fix bug

--- src/gameState.js
+++ src/gameState.js
@@ -16,9 +16,12 @@ function undo(matrix) {
 }
 
 function updatePre(matrix) {
+    if (pre.length === 0)
+        pre = [[], [], [], []]
+
     if (pre.length === 0 || pre.toString() !== matrix.toString()) {
-        for (let i = 0; i < pre.length; i++) {
-            for (let j = 0; j < pre[i].length; j++) {
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix[i].length; j++) {
                 pre[i][j] = matrix[i][j]
             }
         }
