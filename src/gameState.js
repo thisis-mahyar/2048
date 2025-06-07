0cf29ec15ea861600c1a9b7164ad2a00b9319941
Mahyar <mahyarstate@gmail.com>
2025-06-07 18:37:51 +0330

    create refresh

--- src/gameState.js
+++ src/gameState.js
@@ -1,3 +1,14 @@
+function refresh(matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            matrix[i][j] = 0
+        }
+    }
+
+    createNewTile(pre, matrix)
+    display(matrix)
+}
+
 function didWin(matrix) {
     for (let i = 0; i < matrix.length; i++)
         for (let j = 0; j < matrix[i].length; j++)
