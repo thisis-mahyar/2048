6fa9f20e1f70e6a44561d74499c485c0b5a4d115
Mahyar <mahyarstate@gmail.com>
2025-06-08 18:06:46 +0330

    fix bug

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 'use strict'
 
 // only when the page loads, init the game
-window.addEventListener('load', init)
-window.addEventListener('keydown', handleKeydown)
+document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('keydown', handleKeydown)
 
 function init() {
     createNewTile(tiles)
